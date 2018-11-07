8786a64e4b7a3554c9eff72ff5d3ee1504977b2a
Unknown <158840284@qq.com>
2018-11-06 22:33:02 -0600

    make chart as a reusable component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Chart from "../components/chart";
 
 class WeatherList extends Component {
   renderWeather(cityData) {
+    const temps = cityData.list.map(weather => weather.main.temp);
     return (
       <tr key={cityData.city.name}>
         <td>{cityData.city.name}</td>
+        <td>
+          <Chart data={temps} color="orange" />
+        </td>
       </tr>
     );
   }
